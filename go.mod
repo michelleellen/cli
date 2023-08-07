fde45817ddd65527e73d561d0d9db9cd15a1136a
Pantani <Pantani>
2023-08-08 00:47:07 +0200

    ädd hermes relayer

--- go.mod
+++ go.mod
@@ -37,6 +37,7 @@ require (
 	github.com/hashicorp/go-hclog v1.2.0
 	github.com/hashicorp/go-plugin v1.4.9
 	github.com/iancoleman/strcase v0.2.0
+	github.com/ignite/ignite-files v0.0.0
 	github.com/ignite/web v0.4.3
 	github.com/imdario/mergo v0.3.15
 	github.com/jpillora/chisel v1.8.1
@@ -54,13 +55,13 @@ require (
 	github.com/rs/cors v1.9.0
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/pflag v1.0.5
-	github.com/stretchr/testify v1.8.2
+	github.com/stretchr/testify v1.8.4
 	github.com/tbruyelle/mdgofmt v0.1.3
 	github.com/vektra/mockery/v2 v2.27.1
 	go.etcd.io/bbolt v1.3.7
 	golang.org/x/exp v0.0.0-20230519143937-03e91628a987
 	golang.org/x/mod v0.10.0
-	golang.org/x/sync v0.2.0
+	golang.org/x/sync v0.3.0
 	golang.org/x/term v0.8.0
 	golang.org/x/text v0.9.0
 	golang.org/x/tools v0.9.1
@@ -75,6 +76,7 @@ replace (
 	github.com/99designs/keyring => github.com/cosmos/keyring v1.2.0
 	github.com/dgrijalva/jwt-go => github.com/golang-jwt/jwt/v4 v4.4.2
 	github.com/gin-gonic/gin => github.com/gin-gonic/gin v1.8.1
+	github.com/ignite/ignite-files => ../ignite-files
 	github.com/syndtr/goleveldb => github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7
 )
 
---
+++ ignite/pkg/hermes/hermes.go
@@ -0,0 +1,158 @@
+package hermes
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/ignite/ignite-files/hermes"
+
+	"github.com/ignite/cli/ignite/pkg/cmdrunner/exec"
+	"github.com/ignite/cli/ignite/pkg/localfs"
+)
+
+const (
+	FlagHostChain        = "host-chain"
+	FlagReferenceChain   = "reference-chain"
+	FlagChainA           = "a-chain"
+	FlagChainB           = "b-chain"
+	FlagClientA          = "a-client"
+	FlagClientB          = "b-client"
+	FlagConnectionA      = "a-connection"
+	FlagConnectionB      = "b-connection"
+	FlagPortA            = "a-port"
+	FlagPortB            = "b-port"
+	FlagShowCounterparty = "show-counterparty"
+	FlagChain            = "chain"
+)
+
+const (
+	// CommandCreate is the Hermes create command.
+	CommandCreate CommandName = "create"
+
+	// CommandQuery is the Hermes query command.
+	CommandQuery CommandName = "query"
+
+	// CommandStart is the Hermes start command.
+	CommandStart CommandName = "start"
+
+	// CommandClient is the Hermes client command.
+	CommandClient CreateCommand = "client"
+
+	// CommandConnection is the Hermes connection command.
+	CommandConnection CreateCommand = "connection"
+
+	// CommandChannel is the Hermes channel command.
+	CommandChannel CreateCommand = "channel"
+
+	// CommandChannels  is the Hermes channels command.
+	CommandChannels QueryCommand = "channels"
+)
+
+type (
+	// Flags represents the Hermes run flags.
+	Flags map[string]interface{}
+	// CommandName represents a high level command under Hermes.
+	CommandName string
+	// QueryCommand represents the query command under Hermes.
+	QueryCommand string
+	// CreateCommand represents the create command under Hermes.
+	CreateCommand string
+
+	Hermes struct {
+		path    string
+		binary  []byte
+		cleanup func()
+	}
+
+	// Option configures Generate configs.
+	Option func(*configs)
+
+	// Configs holds Generate configs.
+	configs struct {
+		flags Flags
+	}
+)
+
+// WithFlags assigns the command flags.
+func WithFlags(flags Flags) Option {
+	return func(c *configs) {
+		c.flags = flags
+	}
+}
+
+// New returns the hermes binary executable.
+func New() (*Hermes, error) {
+	// untar the binary.
+	gzr, err := gzip.NewReader(bytes.NewReader(hermes.Binary()))
+	if err != nil {
+		panic(err)
+	}
+	defer gzr.Close()
+
+	tr := tar.NewReader(gzr)
+
+	if _, err := tr.Next(); err != nil {
+		return nil, err
+	}
+
+	binary, err := io.ReadAll(tr)
+	if err != nil {
+		return nil, err
+	}
+
+	path, cleanup, err := localfs.SaveBytesTemp(binary, "hermes", 0o755)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Hermes{
+		path:    path,
+		binary:  binary,
+		cleanup: cleanup,
+	}, nil
+}
+
+func (h *Hermes) Cleanup() error {
+	h.cleanup()
+	h.binary = nil
+	return os.RemoveAll(h.path)
+}
+
+func (h *Hermes) Create(ctx context.Context, cmd CreateCommand, options ...Option) error {
+	return h.RunCmd(ctx, CommandCreate, string(cmd), options...)
+}
+
+func (h *Hermes) Query(ctx context.Context, cmd QueryCommand, options ...Option) error {
+	return h.RunCmd(ctx, CommandQuery, string(cmd), options...)
+}
+
+func (h *Hermes) Start(ctx context.Context, options ...Option) error {
+	return h.RunCmd(ctx, CommandStart, "", options...)
+}
+
+func (h *Hermes) RunCmd(ctx context.Context, command CommandName, subCommand string, options ...Option) error {
+	c := configs{}
+	for _, o := range options {
+		o(&c)
+	}
+
+	cmd := []string{h.path, string(command)}
+	if subCommand != "" {
+		cmd = append(cmd, subCommand)
+	}
+	for flag, value := range c.flags {
+		if v, ok := value.(bool); ok && v {
+			cmd = append(cmd, fmt.Sprintf("--%s", flag))
+		} else {
+			cmd = append(cmd, fmt.Sprintf("--%s=%s", flag, value))
+		}
+	}
+
+	// execute the command.
+	return exec.Exec(ctx, cmd)
+}
